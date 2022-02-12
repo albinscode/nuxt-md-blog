58d41169c15e35cbc2c241082065c1c67aec060f
Albin VIGIER <avigier@linagora.com>
2022-02-12 19:19:23 +0100

    basic non optimized highlighting

--- components/search.vue
+++ components/search.vue
@@ -5,8 +5,17 @@
     </div>
 </template>
 
+<style>
+    .highlight {
+        background-color: yellow;
+    }
+</style>
+
 <script>
 
+    import highlightWords from 'highlight-words';
+
+    // to improve we can use search highlighting with chunks: https://www.npmjs.com/package/highlight-words
     export default {
 
         data() {
@@ -24,9 +33,41 @@
         },
         methods: {
             search: function () {
-                let foundArticles = this.articles
+
+                let foundArticles = this.articles.map(article => {
+                    return {...article}
+                })
                 if (this.term !== '') {
-                    foundArticles = this.filteredArticles.filter( article => article.title.indexOf(this.term) !== -1)
+                    foundArticles = foundArticles.filter( (article) => {
+
+                        // an array of chunks with matching and non matching terms
+                        const chunks = highlightWords({
+                            text: article.content,
+                            query: this.term
+                        });
+
+                        let hasMatch = false
+                        // we concatenate all chunks after having highlighted all matching chunks
+                        const newContent = chunks.reduce( (acc, chunk) => {
+                            if (chunk.match) {
+                                acc += `<span class="highlight">${chunk.text}</span>`
+                                hasMatch = true
+                            }
+                            else {
+                                acc += chunk.text
+                            }
+                            return acc
+                        }, '')
+
+                        if (hasMatch) {
+                            article.content = newContent
+                        }
+                        return hasMatch
+                    })
+
+
+
+
                 }
                 this.$store.commit('setFilteredArticles', foundArticles)
             }
